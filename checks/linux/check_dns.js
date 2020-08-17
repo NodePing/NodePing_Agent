d9288a960e7a74f6836e0b34942fa9fba6687c56
Shawn Parrish <sparrish@nodeping.com>
2020-08-17 18:03:49 +0000

    Fixing tcp query socket cleanup

--- checks/linux/check_dns.js
+++ checks/linux/check_dns.js
@@ -110,6 +110,7 @@ var check = exports.check = function(jobinfo) {
       type: dnstype
     });
     var killit = false;
+    var myanswer, myerr;
     jobinfo.results = {start: new Date().getTime()};
     //logger.log('info','DNS: targetip:'+sys.inspect(jobinfo.parameters.targetip)+', type:'+sys.inspect(dnstype)+', dnstoresolve:'+sys.inspect(dnstoresolve));
     try {
@@ -137,7 +138,6 @@ var check = exports.check = function(jobinfo) {
                 killit = true;
                 if (req) {
                     req.cancel();
-                    req = null;
                 }
                 debugMessage('info',"check_dns: setTimeout of "+timeout.toString()+" called on "+jobinfo._id);
                 jobinfo.results.runtime = jobinfo.results.end - jobinfo.results.start;
@@ -150,146 +150,142 @@ var check = exports.check = function(jobinfo) {
         });
 
         req.on('message', function (err, answer) {
-            jobinfo.results.end = new Date().getTime();
-            jobinfo.results.runtime = jobinfo.results.end - jobinfo.results.start;
-            clearTimeout(timeoutid);
-            if (req) {
-                req = null;
-            }
-            if (err) {
-                debugMessage('error','check_dns: DNS on "error": '+sys.inspect(err.toString()));
-                if (!killit) {
-                    killit = true;
-                    jobinfo.results.statusCode = 'Error';
-                    jobinfo.results.success = false;
-                    jobinfo.results.message = "DNS error "+err.toString();
-                    jobinfo.results.diag.dns.error = err.toString();
-                    resultobj.process(jobinfo);
-                }
-                return true;
-            } else {
-                debugMessage('info','check_dns: resolve response is: '+sys.inspect(answer));
-                clearTimeout(timeoutid);
-                if (!killit) {
-                    killit = true;
-                    // Are we looking for a match?
-                    if (jobinfo.parameters.dnstoresolve && jobinfo.parameters.dnstoresolve != '') {
-                        if (answer.answer.length < 1) {
-                            debugMessage('error','check_dns: Error: Server responded but no result records.');
-                            jobinfo.results.diag.dns.answer = [];
-                            jobinfo.results.success = false;
-                            jobinfo.results.message = 'No Resolution';
-                            jobinfo.results.statusCode = 'Failed';
-                            resultobj.process(jobinfo);
-                            return true;
-                        }
-                        var dnstypeid = answer.question[0].type;
-                        if (jobinfo.parameters.contentstring && jobinfo.parameters.contentstring !== '' && jobinfo.parameters.contentstring != 'Optional') {
-                            debugMessage('info','check_dns: Found Result Records: '+sys.inspect(answer.answer));
-                            // Does it match?
-                            for (var i=0;i<answer.answer.length;i++) {
-                                if (answer.answer[i].type == dnstypeid || dnstype == 'ANY') {
-                                    var found = false;
-                                    if (answer.answer[i].name == jobinfo.parameters.contentstring) {
-                                        debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "name" '+answer.answer[i].name);
-                                        found = true;
-                                    } else if (answer.answer[i].address == jobinfo.parameters.contentstring) {
-                                        debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "address" '+answer.answer[i].address);
-                                        found = true;
-                                    } else if (answer.answer[i].primary == jobinfo.parameters.contentstring) {
-                                        debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "primary" '+answer.answer[i].primary);
-                                        found = true;
-                                    } else if (answer.answer[i].data == jobinfo.parameters.contentstring) {
-                                        debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "data" '+answer.answer[i].data);
-                                        found = true;
-                                    } else if (answer.answer[i].exchange == jobinfo.parameters.contentstring) {
-                                        debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "exchange" '+answer.answer[i].exchange);
-                                        found = true;
-                                    } else if (answer.answer[i].target == jobinfo.parameters.contentstring) {
-                                        debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "target" '+answer.answer[i].target);
-                                        found = true;
-                                    }
-                                    if (found) {
-                                        req = null;
-                                        debugMessage('info','check_dns: We found '+jobinfo.parameters.contentstring);
-                                        jobinfo.results.success = true;
-                                        jobinfo.results.statusCode = 'Success';
-                                        if (jobinfo.parameters.verify && jobinfo.parameters.verify !== 'false') {
-                                            return dnssecCheck(jobinfo);
-                                        }
-                                        resultobj.process(jobinfo);
-                                        return true;
-                                    }
-                                }
-                                debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' does not match '+sys.inspect(answer.answer[i.toString()]));
-                            }
-                            // No match, fail
-                            req = null;
-                            jobinfo.results.success = false;
-                            jobinfo.results.message = 'DNS does not match';
-                            jobinfo.results.statusCode = 'Failure';
-                            jobinfo.results.diag.dns.answer = prepAnswerForDiags(answer.answer);
-                            jobinfo.results.diag.dns.expected = jobinfo.parameters.contentstring;
-                            resultobj.process(jobinfo);
-                            return true;
-                        } else {
-                            // Checking to resolve, but not looking for anything particular...
-                            // But we need to make sure it returned something for what we asked for.
-                            // Cause google will return an entry for the 'base' if the domain doesn't exist.
-                            var found = false;
-                            for (var i=0;i<answer.answer.length;i++) {
-                                if (answer.answer[i].type == dnstypeid || dnstype == 'ANY') {
-                                    if (answer.answer[i].name == jobinfo.parameters.dnstoresolve) {
-                                        found = true;
-                                        break;
-                                    }
-                                }
-                            }
-                            if (found) {
-                                debugMessage('info','check_dns: We found '+jobinfo.parameters.contentstring);
-                                jobinfo.results.success = true;
-                                jobinfo.results.statusCode = 'Success';
-                                jobinfo.results.message = 'Domain found';
-                                if (jobinfo.parameters.verify && jobinfo.parameters.verify !== 'false') {
-                                    return dnssecCheck(jobinfo);
-                                }
-                            } else {
-                                debugMessage('info','check_dns: We found no result for '+jobinfo.parameters.dnstoresolve);
-                                jobinfo.results.success = false;
-                                jobinfo.results.statusCode = 'Fail';
-                                jobinfo.results.message = 'Domain not found';
-                                jobinfo.results.diag.dns.answer = prepAnswerForDiags(answer.answer);
-                            }
-                            req = null;
-                            resultobj.process(jobinfo);
-                            return true;
-                        }
-                    } else {
-                        // We're not looking to resolve anything particular, just checking the server.
-                        debugMessage('info','check_dns: Server responded: '+sys.inspect(answer.answer));
-                        req = null;
-                        jobinfo.results.success = true;
-                        jobinfo.results.statusCode = 'Success';
-                        jobinfo.results.message = 'Server Responded';
-                        resultobj.process(jobinfo);
-                        return true;
-                    }
-                }
-                return true;
-            }
+            debugMessage('info','check_dns: message is: '+sys.inspect(answer));
+            myanswer = answer;
+            myerr = err;
         });
         req.on('cancelled', function () {
             debugMessage('info','check_dns: cancelled');
         });
         req.on('end', function () {
             debugMessage('info','check_dns: End');
+            if (killit) {
+                return false;
+            }
+            killit = true;
+            clearTimeout(timeoutid);
+            jobinfo.results.end = new Date().getTime();
+            jobinfo.results.runtime = jobinfo.results.end - jobinfo.results.start;
+            req.cancel();
+            if (myanswer && myanswer._socket && myanswer._socket._socket && myanswer._socket._socket.readable) {
+                myanswer._socket.close(); // killing a tcp connection
+            }
+            if (myerr) {
+                debugMessage('error','check_dns: DNS on "error": '+sys.inspect(err.toString()));
+                jobinfo.results.statusCode = 'Error';
+                jobinfo.results.success = false;
+                jobinfo.results.message = "DNS error "+err.toString();
+                jobinfo.results.diag.dns.error = err.toString();
+                resultobj.process(jobinfo);
+                return true;
+            } else {
+                debugMessage('info','check_dns: resolve response is: '+sys.inspect(myanswer));
+                // Are we looking for a match?
+                if (jobinfo.parameters.dnstoresolve && jobinfo.parameters.dnstoresolve != '') {
+                    if (myanswer.answer.length < 1) {
+                        debugMessage('error','check_dns: Error: Server responded but no result records.');
+                        jobinfo.results.diag.dns.answer = [];
+                        jobinfo.results.success = false;
+                        jobinfo.results.message = 'No Resolution';
+                        jobinfo.results.statusCode = 'Failed';
+                        resultobj.process(jobinfo);
+                        return true;
+                    }
+                    var dnstypeid = myanswer.question[0].type;
+                    if (jobinfo.parameters.contentstring && jobinfo.parameters.contentstring !== '' && jobinfo.parameters.contentstring != 'Optional') {
+                        debugMessage('info','check_dns: Found Result Records: '+sys.inspect(myanswer.answer));
+                        // Does it match?
+                        for (var i=0;i<myanswer.answer.length;i++) {
+                            if (myanswer.answer[i].type == dnstypeid || dnstype == 'ANY') {
+                                var found = false;
+                                if (myanswer.answer[i].name == jobinfo.parameters.contentstring) {
+                                    debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "name" '+myanswer.answer[i].name);
+                                    found = true;
+                                } else if (myanswer.answer[i].address == jobinfo.parameters.contentstring) {
+                                    debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "address" '+myanswer.answer[i].address);
+                                    found = true;
+                                } else if (myanswer.answer[i].primary == jobinfo.parameters.contentstring) {
+                                    debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "primary" '+myanswer.answer[i].primary);
+                                    found = true;
+                                } else if (myanswer.answer[i].data == jobinfo.parameters.contentstring) {
+                                    debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "data" '+myanswer.answer[i].data);
+                                    found = true;
+                                } else if (myanswer.answer[i].exchange == jobinfo.parameters.contentstring) {
+                                    debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "exchange" '+myanswer.answer[i].exchange);
+                                    found = true;
+                                } else if (myanswer.answer[i].target == jobinfo.parameters.contentstring) {
+                                    debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' equals "target" '+myanswer.answer[i].target);
+                                    found = true;
+                                }
+                                if (found) {
+                                    debugMessage('info','check_dns: We found '+jobinfo.parameters.contentstring);
+                                    jobinfo.results.success = true;
+                                    jobinfo.results.statusCode = 'Success';
+                                    if (jobinfo.parameters.verify && jobinfo.parameters.verify !== 'false') {
+                                        return dnssecCheck(jobinfo);
+                                    }
+                                    resultobj.process(jobinfo);
+                                    return true;
+                                }
+                            }
+                            debugMessage('info','check_dns: '+jobinfo.parameters.contentstring+' does not match '+sys.inspect(myanswer.answer[i.toString()]));
+                        }
+                        // No match, fail
+                        jobinfo.results.success = false;
+                        jobinfo.results.message = 'DNS does not match';
+                        jobinfo.results.statusCode = 'Failure';
+                        jobinfo.results.diag.dns.answer = prepAnswerForDiags(myanswer.answer);
+                        jobinfo.results.diag.dns.expected = jobinfo.parameters.contentstring;
+                        resultobj.process(jobinfo);
+                        return true;
+                    } else {
+                        // Checking to resolve, but not looking for anything particular...
+                        // But we need to make sure it returned something for what we asked for.
+                        // Cause google will return an entry for the 'base' if the domain doesn't exist.
+                        var found = false;
+                        for (var i=0;i<myanswer.answer.length;i++) {
+                            if (myanswer.answer[i].type == dnstypeid || dnstype == 'ANY') {
+                                if (myanswer.answer[i].name == jobinfo.parameters.dnstoresolve) {
+                                    found = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (found) {
+                            debugMessage('info','check_dns: We found '+jobinfo.parameters.contentstring);
+                            jobinfo.results.success = true;
+                            jobinfo.results.statusCode = 'Success';
+                            jobinfo.results.message = 'Domain found';
+                            if (jobinfo.parameters.verify && jobinfo.parameters.verify !== 'false') {
+                                return dnssecCheck(jobinfo);
+                            }
+                        } else {
+                            debugMessage('info','check_dns: We found no result for '+jobinfo.parameters.dnstoresolve);
+                            jobinfo.results.success = false;
+                            jobinfo.results.statusCode = 'Fail';
+                            jobinfo.results.message = 'Domain not found';
+                            jobinfo.results.diag.dns.answer = prepAnswerForDiags(myanswer.answer);
+                        }
+                        resultobj.process(jobinfo);
+                        return true;
+                    }
+                } else {
+                    // We're not looking to resolve anything particular, just checking the server.
+                    debugMessage('info','check_dns: Server responded: '+sys.inspect(myanswer.answer));
+                    jobinfo.results.success = true;
+                    jobinfo.results.statusCode = 'Success';
+                    jobinfo.results.message = 'Server Responded';
+                    resultobj.process(jobinfo);
+                    return true;
+                }
+            }
         });
         var timeoutid = setTimeout(function() {
             if(!killit){
                 killit = true;
                 if(req){
                     req.cancel();
-                    req = null;
+
                 }
                 debugMessage('info',"check_dns: setTimeout of "+timeout.toString()+" called on "+jobinfo._id);
                 jobinfo.results.end = new Date().getTime();
